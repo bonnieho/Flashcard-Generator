a90fcc9cf23c1453b4c87b180d67eaf0a733cf49
bonnieho <bonnieho@rice.edu>
2017-09-05 23:16:25 -0500

    created basic and cloze js files; incl instructions

---
+++ BasicCard.js
@@ -0,0 +1,32 @@
+// testing that this file is loaded
+console.log('BasicCard is loaded');
+
+module.exports = BasicCard;
+
+
+/*
+    This file should define a Node module that exports a constructor for creating basic flashcards, e.g.: module.exports = BasicCard;
+
+    The constructor should accept two arguments: front and back.
+
+    The constructed object should have a front property that contains the text on the front of the card.
+
+    The constructed object should have a back property that contains the text on the back of the card.
+
+    */
+/*
+
+exports.twitterKeys = {
+  consumer_key: '1p0YmNhhPjH7V5gInpTyyukF3',
+  consumer_secret: 'JV08mkUkhsJEWmkwWSxj88MYkkJBeSdWLLeXURH8nkyPnQWlg9',
+  access_token_key: '899833991691464704-5fsDEdl8QrVBCe4QO2RdpA0xa47lEXJ',
+  access_token_secret: 'zfjHCXwo0MES5OgmfOGxmPxQvmmBQeHfDJHmkSg2wntLk',
+}
+
+
+exports.spotifyKeys = {
+	id: '4a98a7e0b8314a74a5e7de8af60df353',
+	secret: 'e3d5dc6a2a2a4104b7d5461687e932fa',
+}
+
+*/
